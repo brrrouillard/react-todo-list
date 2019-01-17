bc0b98972661fc87be49d76236201b66687eee24
Matthieu Brouillard <mbrouilla@gmail.com>
2019-01-17 17:28:01 +0100

    prod

---
+++ build/precache-manifest.c8600146a9a50c1da024e1ad9536b7a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "723fa90d5e323ed08358",
+    "url": "/static/js/main.723fa90d.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "723fa90d5e323ed08358",
+    "url": "/static/css/main.73758946.chunk.css"
+  },
+  {
+    "revision": "3ffb5542006c5d02b1dc17109d44af84",
+    "url": "/index.html"
+  }
+];

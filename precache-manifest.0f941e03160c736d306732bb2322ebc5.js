38d46b075f49c3fc134dae25558ee06113557a41
Matthieu Brouillard <mbrouilla@gmail.com>
2019-01-18 09:22:27 +0100

    Updates

---
+++ precache-manifest.0f941e03160c736d306732bb2322ebc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9061ff665ca8ca5bb17c",
+    "url": "/react-todo-list/static/js/runtime~main.9061ff66.js"
+  },
+  {
+    "revision": "d973ef0b8dc7d940668a",
+    "url": "/react-todo-list/static/js/main.d973ef0b.chunk.js"
+  },
+  {
+    "revision": "ad961163a52a17acddf4",
+    "url": "/react-todo-list/static/js/1.ad961163.chunk.js"
+  },
+  {
+    "revision": "d973ef0b8dc7d940668a",
+    "url": "/react-todo-list/static/css/main.91057187.chunk.css"
+  },
+  {
+    "revision": "14068fca4d2bba0f71c9de9d20e4de01",
+    "url": "/react-todo-list/index.html"
+  }
+];
